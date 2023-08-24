b62f71aa03a7b4ff5ac85ecaa5df0f879cfd256c
Michael-Melis <michael.melis169@gmail.com>
2023-08-24 20:22:40 +0200

    build: popular jobs

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 import { View, ScrollView, SafeAreaView } from 'react-native';
 import { useState } from 'react';
 import { Stack, useRouter } from 'expo-router';
-
 import { COLORS, icons, images, SIZES } from '../constants';
-
 import {
   Nearbyjobs,
   Popularjobs,
@@ -12,10 +10,33 @@ import {
 } from '../components';
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Lol toto je mega appka</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='60%' />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
